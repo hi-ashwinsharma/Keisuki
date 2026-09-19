197ac8cea05819524771f790a23e84814875acfe
hi.ashwinsharma <hi.ashwinsharma@gmail.com>
2026-09-20 01:57:23 +0530

    fix(android): update compileSdk and targetSdk to 36 for Play Store compliance

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,12 +8,12 @@ plugins {
 
 android {
     namespace = "com.hiashwinsharma.keisuki"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.hiashwinsharma.keisuki"
         minSdk = 26
-        targetSdk = 35
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
 
